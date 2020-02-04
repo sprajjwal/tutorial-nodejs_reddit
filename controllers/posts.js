dd2c36903ed0a56691a717e7adca820de8ec0a31
sprajjwal <shashwatprajjwal@gmail.com>
2020-02-03 17:01:36 -0800

    users can see subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,32 +2,40 @@ const Post = require('../models/post');
 
 module.exports = app => {
   // CREATE
-    app.post("/posts/new", (req, res) => {
-        const post = new Post(req.body);
+  app.post("/posts/new", (req, res) => {
+    const post = new Post(req.body);
+    post.save((err, post) => {
+        return res.redirect(`/`);
+    })
+  });
 
-        post.save((err, post) => {
-            return res.redirect(`/`);
-        })
+  app.get("/", (req, res) => {
+    Post.find({})
+    .then(posts => {
+        res.render("posts-index", {posts});
+    })
+    .catch(err => {
+        console.log('error', err.message);
     });
+  });
 
-    app.get("/", (req, res) => {
-        Post.find({})
-        .then(posts => {
-            res.render("posts-index", {posts});
-        })
-        .catch(err => {
-            console.log('error', err.message);
-        });
-    });
-
-    app.get("/posts/:id", function(req, res) {
-        // LOOK UP THE POST
-        Post.findById(req.params.id)
-          .then(post => {
-            res.render("posts-show", { post });
-          })
-          .catch(err => {
-            console.log(err.message);
-          });
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id)
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
       });
+    });
+  app.get("/n/:subreddit", function(req, res) {
+    Post.find({ subreddit: req.params.subreddit })
+    .then(posts => {
+      res.render("posts-index", { posts });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
 };
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const expressValidator = require('express-validator');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Add after body parser initialization!
 app.use(expressValidator());
 
@@ -20,12 +24,10 @@ require('./data/reddit-db');
 
 
 // app.get('/', (req, res) => res.render('posts-index'))
-app.get('/posts/new', (req, res) => res.render('posts-new'))
+app.get('/post/new', (req, res) => res.render('posts-new'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// Handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+
 
 module.exports = app;
