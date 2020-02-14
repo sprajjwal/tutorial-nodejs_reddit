6e029d97197d41317addae3f9bf64226bd852db7
sprajjwal <shashwatprajjwal@gmail.com>
2020-02-13 19:29:01 -0800

    users can login

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,4 +30,40 @@ module.exports = (app) => {
         res.clearCookie('nToken');
         res.redirect('/');
     })
+
+    // LOGIN
+    app.get('/login', (req, res) => {
+        res.render('login')
+    })
+
+    // LOGIN POST
+    app.post('/login', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({username}, "username password")
+        .then(user => {
+          if (!user) {
+            // User not found
+            return res.status(401).send({ message: "Wrong Username or Password" });
+          }
+          // Check the password
+          user.comparePassword(password, (err, isMatch) => {
+            if (!isMatch) {
+              // Password does not match
+              return res.status(401).send({ message: "Wrong Username or password" });
+            }
+            // Create a token
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+              expiresIn: "60 days"
+            });
+            // Set a cookie and redirect to root
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
 }
